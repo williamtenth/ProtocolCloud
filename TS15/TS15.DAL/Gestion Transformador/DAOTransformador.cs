6231a5be017618dbe89b490f1c52dd617ca8c9b7
William Cuadros <williamtenth@gmail.com>
2015-03-28 23:17:07 -0500

    Se mueve DAOTransformador y CLEAN de solución.
    Se mueve DAOTransformador a la carpeta Gestión Transformador.
    
    Se realiza clean sobre la solución, con lo cual se eliminan objetos que
    genera el IDE. Se deben realizar clean antes de hacer commit SIEMPRE.

---
+++ TS15/TS15.DAL/Gestion Transformador/DAOTransformador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TS15.Common.IService;
+using TS15.Common.Generated;
+using TS15.Common.RawObjects;
+using System.Data.Objects.DataClasses;
+
+namespace TS15.DAL
+{
+    public class DAOTransformador : IGestionable
+    {
+
+        public List<EntityObject> Consultar(dbTS15Entities contexto, RawError error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Actualizar(EntityObject entidad, dbTS15Entities contexto, RawError error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Eliminar(EntityObject entidad, dbTS15Entities contexto, RawError error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Crear(EntityObject entidad, dbTS15Entities contexto, RawError error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EntityObject ConsultarXId(int id, dbTS15Entities contexto, RawError error)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
