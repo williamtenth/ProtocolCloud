b93045635afc4ff32ee4c270724e55b27f888baa
William Cuadros <williamtenth@gmail.com>
2015-04-03 08:37:56 -0500

    Creación del UI de Asignar Transformador
    Se crea el : aspx, cs y el designer.cs

---
+++ TS15/TS15V2.UI/APP/Gestion Transformador/Asignar Transformador.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TS15V2.UI.APP.Gestion_Transformador
+{
+    public partial class Asignar_Transformador : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
