a7e76df4e6bb8a86f914d1fb9f2a14467ca96e5e
jaaguilarg <jamesing.dio@gmail.com>
2015-05-12 22:12:57 -0500

    Correcion UI Gestor Protocolo
    Correcion de los botones en la pagina GestionProtocolo.aspx

--- TS15/TS15V2.UI/APP/dev/GestionProtocolo/GestorProtocolo.aspx.designer.cs
+++ TS15/TS15V2.UI/APP/dev/GestionProtocolo/GestorProtocolo.aspx.designer.cs
@@ -103,24 +103,6 @@ namespace TS15V2.UI.APP.dev.GestionProtocolo {
         protected global::System.Web.UI.WebControls.Button btnBuscar;
         
         /// <summary>
-        /// pnlBotonera control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlBotonera;
-        
-        /// <summary>
-        /// pnlCrear control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlCrear;
-        
-        /// <summary>
         /// btnCrear control.
         /// </summary>
         /// <remarks>
@@ -128,15 +110,6 @@ namespace TS15V2.UI.APP.dev.GestionProtocolo {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnCrear;
-        
-        /// <summary>
-        /// pnlEditar control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlEditar;
         
         /// <summary>
         /// btnTerminar control.
